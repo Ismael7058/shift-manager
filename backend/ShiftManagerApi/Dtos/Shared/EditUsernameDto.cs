611f39750f5cb39123907c5a2101c2bfeb5145d1
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-03-25 14:40:26 -0300

    Elimino la validacion de expresion regular para el Username

--- backend/ShiftManagerApi/Dtos/Shared/EditUsernameDto.cs
+++ backend/ShiftManagerApi/Dtos/Shared/EditUsernameDto.cs
@@ -6,7 +6,6 @@ namespace ShiftManagerApi.Dtos
   {
     [Required]
     [StringLength(70, MinimumLength = 1, ErrorMessage = "Username debe tener entre 1 y 70 caracteres")]
-    [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "Username solo puede contener letras y espacios")]
     public string Username { get; set; } = string.Empty;
   }
 }
