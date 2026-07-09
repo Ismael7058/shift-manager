854697ad2181f0f674117ba0ec4f162e417be885
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-07-08 22:17:45 -0300

    feat: limito la modificación de un servicio de un proveedor

--- backend/ShiftManagerApi/Services/ProviderServiceService.cs
+++ backend/ShiftManagerApi/Services/ProviderServiceService.cs
@@ -146,10 +146,6 @@ namespace ShiftManagerApi.Services
       var providerService = await _context.ProviderService.Include(ps => ps.Service).FirstOrDefaultAsync(ps => ps.ProviderId == userId && ps.ServiceId == serviceId);
       if (providerService == null) throw new InvalidOperationException("Servicio del proveedor no encontrado.");
 
-      var service = await _context.Service.Where(s => s.Id == updateDto.ServiceId).FirstOrDefaultAsync();
-      if (service == null) throw new InvalidOperationException("Servicio no disponible.");
-
-      providerService.ServiceId = updateDto.ServiceId;
       providerService.DurationMinutes = updateDto.DurationMinutes;
       providerService.Price = updateDto.Price;
 
