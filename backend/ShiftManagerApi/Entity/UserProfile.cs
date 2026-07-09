c0ca3dfd2451901f7ff232ea3247a630badce063
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-07-09 00:11:46 -0300

    feat: implementa funcionalidad actualizar y eliminar las imágenes de perfil de un usuairo
    -

--- backend/ShiftManagerApi/Entity/UserProfile.cs
+++ backend/ShiftManagerApi/Entity/UserProfile.cs
@@ -8,10 +8,12 @@ namespace ShiftManagerApi.Entity
     public DateOnly DateOfBirth { get; set; }
     public GenderType Gender { get; set; }
     public string? PhoneNumber { get; set; }
+    public string? PictureURL { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
 
+    public IFormFile? Picture { get; set; } 
     public UserAuth UserAuth { get; set; } = null!;
     public List<ProviderService> ProviderService { get; set; } = new();
   }
