b8b17b15b3b36f865660356c9df38b26aab3939a
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-07-15 20:27:25 -0300

    feat: implementa dtos e interfaz para obtener los clientes

---
+++ backend/ShiftManagerApi/Dtos/Client/ClientDto.cs
@@ -0,0 +1,12 @@
+namespace ShiftManagerApi.Dtos
+{
+  public record ClientDto
+  {
+    public long Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? PictureURL { get; set; }
+  }
+}
