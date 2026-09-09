1bc162af3e464d7190943edddb36f7c6a4fa9d71
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-09-08 23:56:10 -0300

    fix: corrección del formato de body para registrar e iniciar sesión

--- frontend/ShiftManagerApp/src/services/authService.js
+++ frontend/ShiftManagerApp/src/services/authService.js
@@ -11,33 +11,43 @@ export const AuthService = {
       '/login',
       {
         method: 'POST',
-        body: JSON.stringify(credentials)
+        body: JSON.stringify({ Identifier: credentials.identifier, Password: credentials.password })
       }
     ),
 
-    /**
-     * Registrarse como Cliente
-     * @param {RegisterDto} register 
-     * @returns {Promise<{user: UserDto, expiration: string, roleActive: string}>}
-     */
-    register: (register) => 
-      apiFetch(
-        '/register',
-        {
-          method: 'POST',
-          body: JSON.stringify(register)
-        }
-      ),
+  /**
+   * Registrarse como Cliente
+   * @param {RegisterDto} register 
+   * @returns {Promise<{user: UserDto, expiration: string, roleActive: string}>}
+   */
+  register: (register) =>
+    apiFetch(
+      '/register',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          FirstName: register.firstName,
+          LastName: register.lastName,
+          DateOfBirth: register.dateOfBirth,
+          Gender: register.gender,
+          PhoneNumber: register.phoneNumber,
+          Username: register.username,
+          Email: register.email,
+          Password: register.password,
+          ConfirmPassword: register.confirmPassword
+        })
+      }
+    ),
 
-    /**
-     * Cerrar sesion
-     * @returns {Promise<any>}
-     */
-    logout: () =>
-      apiFetch(
-        '/logout',
-        {
-          method: 'POST'
-        }
-      )
+  /**
+   * Cerrar sesion
+   * @returns {Promise<any>}
+   */
+  logout: () =>
+    apiFetch(
+      '/logout',
+      {
+        method: 'POST'
+      }
+    )
 };
